94f55c4ff376435fa78c540a9376a33af7732583
JFincher42 <jfincher42@gmail.com>
2017-07-23 20:14:16 -0500

    Full implementation complete.

--- SpaceNeedle/src/SpaceNeedle.java
+++ SpaceNeedle/src/SpaceNeedle.java
@@ -3,7 +3,41 @@ public class SpaceNeedle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		printSpire();
+		printBodyTop();
+		printBodyBottom();
+		printSpire();
+		printTower();
+		printBodyTop();
+	}
+	
+	public static void printSpire()
+	{
+		for (int i=0; i<4; i++)
+			System.out.println("            ||");
+
+	}
+	
+	public static void printBodyTop()
+	{
+		System.out.println("         __/||\\__"); 
+		System.out.println("      __/:::||:::\\__");
+		System.out.println("   __/::::::||::::::\\__");
+		System.out.println("__/:::::::::||:::::::::\\__");
+		System.out.println("|\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"|"); 
+	}
+
+	public static void printBodyBottom()
+	{
+		System.out.println("\\_/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\_/"); 
+		System.out.println(	"  \\_/\\/\\/\\/\\/\\/\\/\\/\\/\\_/"); 
+		System.out.println("    \\_/\\/\\/\\/\\/\\/\\/\\_/"); 
+		System.out.println("      \\_/\\/\\/\\/\\/\\_/");
+	}
+	public static void printTower()
+	{
+		for (int i=0; i<16; i++)
+			System.out.println("         |%%||%%|");
 	}
 
 }
